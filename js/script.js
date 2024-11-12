2860b5e868bb4f72ff544710afd7218539396baf
Filipp Orekhov <orekhov_splav@bk.ru>
2024-11-13 01:36:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const circleCount = 10;
+const mySkills = [
+    {name: 'Soft', countFilled: 5,},
+    {name: 'Html', countFilled: 4,},
+    {name: 'Css', countFilled: 3,},
+    {name: 'Js', countFilled: 2,},
+    {name: 'Figma', countFilled: 1,}
+    ];
+const circles = [];
+
+// const fillCircles = (circlesNode, fillCount) => {
+//     const circles = circlesNode.querySelectorAll('.circle');
+//     for( let i = 0; i < fillCount; i++) {
+//         return circles[i].classList.add('circle_filled');
+//     }
+// }
+
+// const drawCircles = (parentNode, circleCount) => {
+//     for( let i = 0; i < circleCount; i++) {
+//         const circle = document.createElement('div')
+//         circle.classList.add('circle');
+//         return parentNode.append('.circle');
+//     }
+// }
+
+// document.querySelectorAll('.skill').forEach(skill => {
+//     const name = skill.dataset.name;
+//     drawCircles(skill.querySelector('.circles'), circleCount);
+//     fillCircles(skill, mySkills[countFilled]);
+// })
+
+// console.log(drawCircles(skill.querySelector('.circles'), circleCount));
+
+
+let div = document.querySelectorAll('')
